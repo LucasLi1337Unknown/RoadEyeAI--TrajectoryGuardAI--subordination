8680f8d2a84051d581fc59cd7b4d2e510265af2f
Lucas Li <bojun792@gmail.com>
2026-09-18 20:29:56 +0800

    Update constant_velocity.py

--- src/prediction/constant_velocity.py
+++ src/prediction/constant_velocity.py
@@ -1,12 +1,12 @@
 class ConstantVelocityPredictor:
-    def __init__(self, horizonFrames=18):
-        self.horizonFrames = horizonFrames
+    def __init__(self, horizonSeconds=1.0):
+        self.horizonSeconds = horizonSeconds
 
     def predict(self, detection):
         x, y = detection.center
         vx, vy = detection.velocity
         detection.predictedCenter = (
-            x + vx * self.horizonFrames,
-            y + vy * self.horizonFrames
+            x + vx * self.horizonSeconds,
+            y + vy * self.horizonSeconds
         )
         return detection
